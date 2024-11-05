981ddd8edcbccf6f8ed4231d2d16af5ba8b96d31
Payal120324 <146610738+Payal120324@users.noreply.github.com>
2024-11-05 15:01:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+
+/*----- MENU -----*/
+const showMenu = (toggleId,navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*----- CAMBIO COLORS -----*/
+const sizes = document.querySelectorAll('.size__tallas');
+const colors = document.querySelectorAll('.tshirt__color');
+const tshirt = document.querySelectorAll('.tshirt__img');
+
+function changeSize(){
+    sizes.forEach(size => size.classList.remove('active'));
+    this.classList.add('active');
+}
+
+function changeColor(){
+    let primary = this.getAttribute('primary');
+    let color = this.getAttribute('color');
+    let tshirtColor = document.querySelector(`.tshirt__img[color="${color}"]`);
+
+    colors.forEach(c => c.classList.remove('active'));
+    this.classList.add('active');
+
+    document.documentElement.style.setProperty('--primary', primary);
+
+    tshirt.forEach(s => s.classList.remove('shows'));
+    tshirtColor.classList.add('shows')
+}
+
+sizes.forEach(size => size.addEventListener('click', changeSize));
+colors.forEach(c => c.addEventListener('click', changeColor));
+
